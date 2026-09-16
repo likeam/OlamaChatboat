cfc237fa0d2a2e80b66dccc80435030cf0462690
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-09-16 22:43:25 +0500

    Removing Error

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -8,9 +8,7 @@ export function ChatMessage({ message }) {
         maxWidth: "90%",
         background: isUser ? "#0a84ff" : "#3a3a3a",
         padding: "1rem 1rem",
-        borderRadius: isUser
-          ? "1rem 1rem 0.5rem 1rem"
-          : "1rem 1rem 1rem 0.5rem",
+        borderRadius: isUser ? "1rem 1rem 1rem 1rem" : "1rem 1rem 1rem 0.5rem",
         wordWrap: "break-word",
         whiteSpace: "pre-wrap",
       }}
